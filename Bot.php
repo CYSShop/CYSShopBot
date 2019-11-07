79fb74e793d1db6c9a8018b2bfff47115e648533
CYSShop <57495191+CYSShop@users.noreply.github.com>
2019-11-07 18:22:08 +0300

    Create Bot.php

---
+++ Bot.php
@@ -0,0 +1,43 @@
+<?php
+$TOKEN = '913263427:AAEF1BV4fTAxci35TboRtSm9iyUz9DCQ9-k';
+$BOT_USERNAME = 'CYSShop_Bot';
+$webhook = 'https://cysshop.000webhostapp.com/cysshop.php';
+
+$url = 'https://api.telegram.org/bot'.$TOKEN.'/';
+
+
+if (($json = valid()) == false) { echo get($url.'setWebhook?url='.$webhook); exit();}
+ 
+$uid = $json['message']['from']['id'];
+$first_name = $json['message']['from']['first_name'];
+$ANSWER = "Ну привет, ".$first_name;
+sendMessage($uid,$ANSWER);
+
+
+function valid() {
+	$request_from_telegram = false;
+	if(isset($_POST)) {
+		$data = file_get_contents("php://input");
+		if (json_decode($data) != null)
+			$request_from_telegram = json_decode($data,1);
+	}
+	return $request_from_telegram;
+}
+
+
+function sendMessage($chat_id,$text,$markup=null)
+{
+	return get($GLOBALS['url'].'sendMessage?chat_id='.$chat_id.'&text='.urlencode($text).'&reply_markup='.$markup.'&parse_mode=Markdown');
+}
+
+function get($url)
+{
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+
+?>
