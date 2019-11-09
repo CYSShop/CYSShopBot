af51e62ccff71f88dcaeff7d78d8d543e2aa6ca6
CYSShop <57495191+CYSShop@users.noreply.github.com>
2019-11-09 13:14:03 +0300

    Update CBuYBot.php

--- CBuYBot.php
+++ CBuYBot.php
@@ -1,111 +1,43 @@
 <?php
-ini_set('log_errors', 'On');
-ini_set('error_log', 'php_errors.log');
-
-
 $TOKEN = '838275789:AAHY3XDl8Y_hhT5m6FA_dDAvVZERFqFFdA0';
 $BOT_USERNAME = 'CBuYBot';
-$webhook = 'https://github.com/CYSShop/CYSShopBot/blob/master/CBuYBot.php'
+$webhook = 'https://github.com/CYSShop/CYSShopBot/blob/master/CBuYBot.php';
+
 $url = 'https://api.telegram.org/bot'.$TOKEN.'/';
-echo get($url.'setWebhook?url='.$webhook);
 
 
-if (($json = valid()) == false) { echo "Hi, CYSShop_Bot"; exit(); }
-
-	$uid = $json['message']['from']['id'];        
-	$first_name = $json['message']['from']['first_name'];
-
-	$ANSWER = "Приветствую, ".$first_name;
-
-
-	$text = $json['message']['text'];
-
-  switch($text){
-      
-    case 'Мануалы':
-      $ANSWER = "Тут будет раздел мануалов";
-      
-    break;
-        
-    case 'Контакты':
-      $ANSWER = "Почему-то пока не работает";
-      
-    break;
-      
-    case 'Схемы':
-      $ANSWER = "Тут будут платные схемы";
-      
-    break;
-    
-    case 'Канал':
-      $ANSWER = "Тут будет кнопка для перехода на канал ТГ";
-      
-    break;
-      
-    case 'Помощь':
-      $ANSWER = "Здесь будет помощь";
-      
-    break;      
-      
-case '/start':
-      $ANSWER = "Рад поприветствовать тебя в боте канала CYS - CreateYourSelf
-      Список комманд представлен в виде кнопок ниже";
-      $keyboard = keyboard();
-        
-break;      
-  }
-
-sendMessage($uid,$ANSWER, $keyboard);
-
-function keyboard() {
-  
-  var_dump($keyboard = json_encode($keyboard = ['keyboard' => [
-   ['Схемы','Мануалы','Канал'],
-    ['Помощь','Контакты']
-  ] ,
-  'resize_keyboard' => true,
-  'one_time_keyboard' => false,
-  'selective' => true
-  ]),true);
-
-  return $keyboard;
-}
-
-function delete_keyboard()
-{
-  var_dump($keyboard = json_encode($keyboard =  array('remove_keyboard' => true)));
-  return $keyboard;
-}
+if (($json = valid()) == false) { echo get($url.'setWebhook?url='.$webhook); exit();}
+ 
+$uid = $json['message']['from']['id'];
+$first_name = $json['message']['from']['first_name'];
+$ANSWER = "Ну привет, ".$first_name;
+sendMessage($uid,$ANSWER);
 
 
 function valid() {
-  $request_from_telegram = false;
-  if(isset($_POST)) {
+	$request_from_telegram = false;
+	if(isset($_POST)) {
+		$data = file_get_contents("php://input");
+		if (json_decode($data) != null)
+			$request_from_telegram = json_decode($data,1);
+	}
+	return $request_from_telegram;
+}
 
-    $data = file_get_contents("php://input");
-      if (json_decode($data) != null)
-        $request_from_telegram = json_decode($data,1);
-  }
-  return $request_from_telegram;
-} 
 
-function sendMessage($chat_id,$text,$markup=null) 
-{ 
+function sendMessage($chat_id,$text,$markup=null)
+{
+	return get($GLOBALS['url'].'sendMessage?chat_id='.$chat_id.'&text='.urlencode($text).'&reply_markup='.$markup.'&parse_mode=Markdown');
+}
 
-  return get($GLOBALS['url'].'sendMessage?chat_id='.$chat_id.'&text='.urlencode($text).'&reply_markup='.$markup.'&parse_mode=Markdown'); 
-
-} 
-
-function get($url) 
-{ 
-$ch = curl_init($url); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-curl_setopt($ch, CURLOPT_HEADER, 0); 
-$data = curl_exec($ch); 
-curl_close($ch); 
-return $data; 
-} 
- 
- 
+function get($url)
+{
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
 
 ?>
